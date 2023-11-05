ab2efcd81746cc8b81ad50de9e26a29625c3fdb2
feargrinn <tobyrogers000@gmail.com>
2023-11-05 16:39:08 +0100

    06_Pyramid done

---
+++ src/Assignments/06_Pyramid/app.cpp
@@ -0,0 +1,161 @@
+#include "app.h"
+
+#include <vector>
+
+#include "spdlog/spdlog.h"
+
+#include "glad/gl.h"
+
+#include "Application/utils.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/constants.hpp"
+
+#include "glm/gtc/matrix_transform.hpp"
+
+void SimpleShapeApplication::init() {
+    /*
+     * A utility function that reads the shaders' source files, compiles them and creates the program object,
+     * as everything in OpenGL we reference the program by an integer "handle".
+     * The input to this function is a map that associates a shader type (GL_VERTEX_SHADER and GL_FRAGMENT_SHADER) with
+     * its source file. The source files are located in the PROJECT_DIR/shaders directory, where  PROJECT_DIR is the
+     * current assignment directory (e.g., src/Assignments/Triangle).
+     */
+    auto program = xe::utils::create_program(
+            {
+                    {GL_VERTEX_SHADER,   std::string(PROJECT_DIR) + "/shaders/base_vs.glsl"},
+                    {GL_FRAGMENT_SHADER, std::string(PROJECT_DIR) + "/shaders/base_fs.glsl"}
+            });
+
+
+    if (!program) {
+        SPDLOG_CRITICAL("Invalid program");
+        exit(-1);
+    }
+
+    // A vector containing the x,y,z vertex coordinates and rgb color values for the triangle.
+    std::vector<GLfloat> vertices = {
+            -0.5f, -0.5f, 0.0f, 0.5f, 0.5f, 0.5f, // 0
+            -0.5f, 0.5f, 0.0f, 0.5f, 0.5f, 0.5f, // 1
+            0.5f, 0.5f, 0.0f, 0.5f, 0.5f, 0.5f, // 2
+            0.5f, -0.5f, 0.0f, 0.5f, 0.5f, 0.5f, // 3
+
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // 4
+            0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // 5
+            0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, // 6
+
+            -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // 7
+            -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // 8
+            0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // 9
+
+            -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, // 10
+            0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, // 11
+            0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, // 12
+
+            0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // 13
+            -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // 14
+            0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // 15
+            };
+
+    // index vector
+    std::vector<GLubyte> indices = {
+        0,1,2,
+        0,2,3,
+        4,5,6,
+        7,8,9,
+        10,11,12,
+        13,14,15
+        };
+
+    /*
+     * All the calls to the OpenGL API are "encapsulated" in the OGL_CALL macro for debugging purposes as explained in
+     * Assignments/DEBUGGING.md. The macro is defined in src/Application/utils.h. If the call to the OpenGL API returns an
+     * error code, the macro will print the name of the function that failed, the file and line number where the error
+     * occurred.
+     */
+
+    // Generating the buffer and loading the vertex data into it.
+    GLuint v_buffer_handle;
+    OGL_CALL(glGenBuffers(1, &v_buffer_handle));
+    OGL_CALL(glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle));
+    OGL_CALL(glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW));
+    OGL_CALL(glBindBuffer(GL_ARRAY_BUFFER, 0));
+
+    // generating buffer for indices
+    GLuint index_buffer_handle;
+    OGL_CALL(glGenBuffers(1, &index_buffer_handle));
+    OGL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_handle));
+    OGL_CALL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLbyte), indices.data(), GL_STATIC_DRAW));
+    OGL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+    // generating buffer for transformations
+    GLuint transformations_buffer_handle;
+    OGL_CALL(glGenBuffers(1, &transformations_buffer_handle));
+    OGL_CALL(glBindBuffer(GL_UNIFORM_BUFFER, transformations_buffer_handle));
+    OGL_CALL(glBufferData(GL_UNIFORM_BUFFER, 16 * sizeof(float), nullptr, GL_STATIC_DRAW));
+
+    // This set up an OpenGL viewport of the size of the whole rendering window.
+    auto [w, h] = frame_buffer_size();
+    OGL_CALL(glViewport(0, 0, w, h));
+
+    // PVM matrix
+    glm::mat4 PVM(1.0f);
+    glm::mat4 M(1.0);
+    glm::mat4 V = glm::lookAt(glm::vec3(0.0f,0.0f,2.0f),glm::vec3(0.0f,0.0f,0.0f),glm::vec3(0.0f,1.0f,0.0f));
+    glm::mat4 P = glm::perspective(glm::radians(45.0f),float(w)/h,0.1f,20.0f);
+    PVM = P*V*M;
+
+    // binding buffer for transformations
+    OGL_CALL(glBindBufferBase(GL_UNIFORM_BUFFER, 1, transformations_buffer_handle));
+    OGL_CALL(glBufferSubData(GL_UNIFORM_BUFFER, 0, 16 * sizeof(float), &PVM[0]));
+    OGL_CALL(glBindBuffer(GL_UNIFORM_BUFFER, 0));
+
+    // This sets up a Vertex Array Object (VAO) that encapsulates
+    // the state of all vertex buffers needed for rendering.
+    // The vao_ variable is a member of the SimpleShapeApplication class and is defined in src/Application/app.h.
+    OGL_CALL(glGenVertexArrays(1, &vao_));
+    OGL_CALL(glBindVertexArray(vao_));
+    OGL_CALL(glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle));
+
+    /*
+     * The following lines bound the vertex attribute 0 to the currently bound vertex buffer (the one we just created).
+     * Attribute 0 is specified in the vertex shader with the
+     * layout (location = 0) in vec4 a_vertex_position;
+     * directive.
+     */
+    // This specifies that the data for attribute 0 should be read from a vertex buffer
+    OGL_CALL(glEnableVertexAttribArray(0));
+    // and this specifies the data layout in the buffer.
+    OGL_CALL(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
+                                   reinterpret_cast<GLvoid *>(0)));
+
+    // vertices colors
+    OGL_CALL(glEnableVertexAttribArray(1));
+    OGL_CALL(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
+                                   reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat))));
+
+    // binding indices buffer again
+    OGL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_handle));
+
+    OGL_CALL(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    OGL_CALL(glBindVertexArray(0));
+
+    // Setting the background color of the rendering window,
+    // I suggest not using white or black for better debugging.
+    OGL_CALL(glClearColor(0.81f, 0.81f, 0.8f, 1.0f));
+
+    OGL_CALL(glUseProgram(program));
+
+    glEnable(GL_CULL_FACE);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+    // glDepthFunc(GL_ALWAYS); 
+}
+
+//This functions is called every frame and does the actual rendering.
+void SimpleShapeApplication::frame() {
+    // Binding the VAO will set up all the required vertex attribute arrays.
+    OGL_CALL(glBindVertexArray(vao_));
+    OGL_CALL(glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_BYTE, reinterpret_cast<GLvoid *>(0)));
+    OGL_CALL(glBindVertexArray(0));
+}
---
+++ src/Assignments/06_Pyramid/shaders/base_fs.glsl
@@ -0,0 +1,9 @@
+#version 420
+
+layout(location=0) out vec4 vFragColor;
+
+in vec4 vertex_color;
+
+void main() {
+    vFragColor = vertex_color;
+}
